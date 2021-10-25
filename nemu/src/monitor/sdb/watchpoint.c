45f3800538d3e386872f0b514e9cb4728add973d
Diana <925663486@qq.com>
2021-10-25 19:19:37 +0800

    gia

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -5,10 +5,13 @@
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-
+  char str[40];
   /* TODO: Add more members if necessary */
 
 } WP;
+
+WP* new_wp();
+void free_wp(WP *wp);
 
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
@@ -23,6 +26,34 @@ void init_wp_pool() {
   head = NULL;
   free_ = wp_pool;
 }
+WP* new_wp(){
+  if(free_ == NULL)assert(0);
+  else{
+    WP *p = free_ ;
+    free_ = free_ -> next;
+    p -> next = head;
+    head = p;
+    return p; 
+  }
+}
+void free_wp(WP *wp){
+  WP* p = head;
+  //printf("%d\n",wp->NO); 
+  if(p->NO==wp->NO){
+  	head = p->next;
+  	wp->next = free_;
+  	free_=wp;
+  	return ;
+  }
+  while(p->next != NULL){
+  	if(p->next->NO == wp->NO)break;
+  	p=p->next;
+  }
+  p->next =p->next->next;
+  wp->next = free_;
+  free_ = wp;
+}
+
 
 /* TODO: Implement the functionality of watchpoint */
 
